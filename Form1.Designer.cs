15b6520fed7dc44e9a4cb90347d24a48a1325f40
serdararikann <77884239+serdararikann@users.noreply.github.com>
2021-01-23 15:49:09 +0300

    Tez Şablon Kontrolü

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Tez_Sablon_Kontrolu
+{
+    partial class TezSablon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ac = new System.Windows.Forms.Button();
+            this.kontrol = new System.Windows.Forms.Button();
+            this.cikis = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label = new System.Windows.Forms.Label();
+            this.dosya_label = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // ac
+            // 
+            this.ac.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ac.Location = new System.Drawing.Point(10, 12);
+            this.ac.Name = "ac";
+            this.ac.Size = new System.Drawing.Size(85, 49);
+            this.ac.TabIndex = 0;
+            this.ac.Text = "Aç";
+            this.ac.UseVisualStyleBackColor = true;
+            this.ac.Click += new System.EventHandler(this.ac_Click);
+            // 
+            // kontrol
+            // 
+            this.kontrol.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.kontrol.Location = new System.Drawing.Point(187, 12);
+            this.kontrol.Name = "kontrol";
+            this.kontrol.Size = new System.Drawing.Size(85, 49);
+            this.kontrol.TabIndex = 1;
+            this.kontrol.Text = "Kontrol Et";
+            this.kontrol.UseVisualStyleBackColor = true;
+            this.kontrol.Click += new System.EventHandler(this.kontrol_Click);
+            // 
+            // cikis
+            // 
+            this.cikis.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cikis.Location = new System.Drawing.Point(358, 12);
+            this.cikis.Name = "cikis";
+            this.cikis.Size = new System.Drawing.Size(85, 49);
+            this.cikis.TabIndex = 2;
+            this.cikis.Text = "Çıkış";
+            this.cikis.UseVisualStyleBackColor = true;
+            this.cikis.Click += new System.EventHandler(this.cikis_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Bold);
+            this.label.Location = new System.Drawing.Point(9, 72);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(120, 17);
+            this.label.TabIndex = 4;
+            this.label.Text = "Seçilen Dosya:";
+            // 
+            // dosya_label
+            // 
+            this.dosya_label.AutoSize = true;
+            this.dosya_label.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Bold);
+            this.dosya_label.Location = new System.Drawing.Point(129, 74);
+            this.dosya_label.Name = "dosya_label";
+            this.dosya_label.Size = new System.Drawing.Size(0, 13);
+            this.dosya_label.TabIndex = 5;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold);
+            this.richTextBox1.Location = new System.Drawing.Point(10, 92);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(433, 327);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(10, 425);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(433, 36);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // TezSablon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 474);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.dosya_label);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.cikis);
+            this.Controls.Add(this.kontrol);
+            this.Controls.Add(this.ac);
+            this.Name = "TezSablon";
+            this.Text = "Tez Şablon Kontrolü";
+            this.Load += new System.EventHandler(this.TezSablon_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ac;
+        private System.Windows.Forms.Button kontrol;
+        private System.Windows.Forms.Button cikis;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label dosya_label;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Word = Microsoft.Office.Interop.Word;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace Tez_Sablon_Kontrolu
+{
+    public partial class TezSablon : Form
+    {
+        public TezSablon()
+        {
+            InitializeComponent();
+        }
+
+        private void cikis_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ac_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Metin Dosyaları(*.docx)|*.docx|Tüm Dosyalar|*.*";
+            openFileDialog1.Title = "Dosya Seç";
+            DialogResult buton = openFileDialog1.ShowDialog();
+
+            if (buton == DialogResult.OK)
+            {
+                FileInfo dosya = new FileInfo(openFileDialog1.FileName.ToString());
+                dosyaAdi = openFileDialog1.FileName.ToString();
+                string sonDeger = dosyaAdi.Split('\\').Last();
+                dosya_label.Text = sonDeger;
+                kontrol.Enabled = true;
+            }
+        }
+
+        static string dosyaAdi;
+        static string totaltext;
+        static byte kaynakSayisi;
+
+        private void kontrol_Click(object sender, EventArgs e)
+        {
+            oku();
+            kaynakSay(totaltext);
+            atifKontrol(totaltext);
+            paragrafKontrol(totaltext);
+            kelimeSay(totaltext);
+            onsozKontrol(totaltext);
+            kapsamKontrol(totaltext);
+            denklemKontrol(totaltext);
+        }
+
+        private void TezSablon_Load(object sender, EventArgs e)
+        {
+            kontrol.Enabled = false;
+        }
+
+        public void oku()
+        {
+            Microsoft.Office.Interop.Word.Application word = new Microsoft.Office.Interop.Word.Application();
+            object miss = System.Reflection.Missing.Value;
+            object path = dosyaAdi;
+            object readOnly = true;
+            Microsoft.Office.Interop.Word.Document docs = word.Documents.Open(ref path, ref miss, ref readOnly, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss);
+            for (int i = 0; i < docs.Paragraphs.Count; i++)
+            {
+                totaltext += " \r\n " + docs.Paragraphs[i + 1].Range.Text.ToString();
+            }
+            docs.Close();
+            word.Quit();
+        }
+        public void kaynakSay(string metin)
+        {
+            byte sayi1 = 1, sayi2 = 2;
+            string s1 = "", s2 = "";
+            while (true)
+            {
+                s1 = "[" + sayi1.ToString() + "]";
+                s2 = "[" + sayi2.ToString() + "]";
+                sayi1 = sayi2;
+
+                if (metin.Contains(s1))
+                {
+                    sayi1++;
+
+                }
+                if (metin.Contains(s2))
+                {
+                    sayi2++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            richTextBox1.Text = (--sayi1).ToString() + " adet kaynak bulunmaktadır.\n";
+            kaynakSayisi = sayi1;
+        }
+
+        public void atifKontrol(string metin)
+        {
+            byte sayi = 1;
+            string s1 = "";
+
+            while (sayi < kaynakSayisi)
+            {
+                s1 = "[" + sayi.ToString() + "]";
+                int textUzunluk = metin.Length;
+                int yeniTextUzunluk = metin.Replace(s1, null).Length;
+                int kacDefa = (textUzunluk - yeniTextUzunluk) / s1.Length;
+                if (kacDefa < 2)
+                {
+                    richTextBox1.Text += "\n" + (s1 + ". kaynağa atıf yapılmamıştır.");
+                }
+                sayi++;
+            }
+        }
+
+        public void paragrafKontrol(string metin)
+        {
+            int paraCount = metin.Split('\n').Length;
+            string[] dizi = new string[paraCount];
+            dizi = metin.Split('\n');
+            for (int i = 0; i < paraCount; i++)
+            {
+                char k = char.Parse(dizi[i].Substring(1, 1));
+                bool a = !Char.IsNumber(k);
+                if (dizi[i].Length < 180 && i != 0 && a == true && dizi[i].Length > 1 && k == '\r')
+                {
+                    richTextBox1.Text += "\n" + i + ". paragrafta cümle sayısı ikiden azdır.";
+                }
+            }
+        }
+        public void kelimeSay(string metin)
+        {
+            string s1 = "“";
+            string s2 = "”";
+            int sayac = 0;
+            bool kontrol = false;
+
+            int paraCount = metin.Split('\n').Length;
+            string[] dizi = new string[paraCount];
+            dizi = metin.Split('\n');
+            for (int i = 0; i < paraCount; i++)
+            {
+                if (dizi[i].Contains(s1))
+                {
+                    sayac = dizi[i].Split(' ').Length;
+                    string[] dizi1 = new string[sayac];
+                    dizi1 = dizi[i].Split(' ');
+                    for (int j = 0; j < sayac; j++)
+                    {
+                        if (dizi1[j].Contains(s2))
+                        {
+                            if (j > 50)
+                            {
+                                richTextBox1.Text += "\n\n50 kelimeden fazla alıntı yapılmıştır.";
+                                kontrol = true;
+                                break;
+                            }
+                        }
+                        if (kontrol == true)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void onsozKontrol(string metin)
+        {
+            int paraCount = metin.Split('\n').Length;
+            string[] dizi = new string[paraCount];
+            dizi = metin.Split('\n');
+            for (int i = 0; i < paraCount; i++)
+            {
+                if (dizi[i].Contains("ÖNSÖZ") || dizi[i].Contains("Önsöz"))
+                {
+                    if (dizi[i + 1].Contains("teşekkür") || dizi[i + 1].Contains("Teşekkür"))
+                    {
+                        richTextBox1.Text += "\n\nÖnsöz bölümünün ilk paragrafında teşekkür ifadesi vardır.";
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void kapsamKontrol(string metin)
+        {
+            byte kontrol = 0;
+            int j = 0;
+            int paraCount = metin.Split('\n').Length;
+            string[] dizi = new string[paraCount];
+            dizi = metin.Split('\n');
+            for (int i = 0; i < paraCount; i++)
+            {
+                if (dizi[i].Contains("GİRİŞ") || dizi[i].Contains("Giriş"))
+                {
+                    for (; ; )
+                    {
+                        if (j >= paraCount || dizi[j].Contains("2."))
+                        {
+                            break;
+                        }
+                        else if (dizi[j].Contains("kapsam") || dizi[j].Contains("Kapsam") || dizi[j].Contains("Bu tez çalışmasında") || dizi[j].Contains("Bu tez çalışması") || dizi[j].Contains("Bu tezde") || dizi[j].Contains("Bu çalışmada"))
+                        {
+                            kontrol = 1;
+                            break;
+                        }
+                        else
+                        {
+                            kontrol = 2;
+                        }
+                        j++;
+                    }
+                    break;
+                }
+                if (kontrol == 2)
+                {
+                    break;
+                }
+            }
+            if (kontrol == 2)
+            {
+                richTextBox1.Text += "\n\nGiriş kısmında Kapsam ve Organizasyona yer verilmemiştir.";
+            }
+        }
+
+        public void denklemKontrol(string metin)
+        {
+            int sayi = 2;
+            int j = 0;
+
+            bool kontrol = true;
+            int paraCount = metin.Split('\n').Length;
+            string[] dizi = new string[paraCount];
+            dizi = metin.Split('\n');
+            for (int i = 0; i < paraCount; i++)
+            {
+                string denklem = sayi.ToString() + ". ";
+                string denklem2 = (sayi + 1).ToString() + ". ";
+                if (dizi[i].Contains(denklem))
+                {
+                    j = i;
+                    while (true)
+                    {
+                        if (j >= paraCount)
+                        {
+                            break;
+                        }
+                        if (dizi[j].Contains("Denklem " + (sayi + 1).ToString()) || dizi[j].Contains("Denklem " + (sayi + 2).ToString()) ||
+                            dizi[j].Contains("Denklem " + (sayi + 3).ToString()) || dizi[j].Contains("Denklem " + (sayi + 4).ToString()) ||
+                            dizi[j].Contains("Denklem " + (sayi + 5).ToString()) || dizi[j].Contains("Denklem " + (sayi + 6).ToString()) ||
+                            dizi[j].Contains("Denklem " + (sayi + 7).ToString()) || dizi[j].Contains("Denklem " + (sayi + 8).ToString()) ||
+                            dizi[j].Contains("Denklem " + (sayi + 9).ToString()) || dizi[j].Contains("Denklem " + (sayi + 10).ToString()) ||
+                            dizi[j].Contains("Denklem " + (sayi - 1).ToString()) || dizi[j].Contains("Denklem " + (sayi - 2).ToString()) ||
+                            dizi[j].Contains("Denklem " + (sayi - 5).ToString()) || dizi[j].Contains("Denklem " + (sayi - 6).ToString()) ||
+                            dizi[j].Contains("Denklem " + (sayi - 7).ToString()) || dizi[j].Contains("Denklem " + (sayi - 8).ToString()) ||
+                            dizi[j].Contains("Denklem " + (sayi - 9).ToString()) || dizi[j].Contains("Denklem " + (sayi - 10).ToString()))
+                        {
+                            kontrol = false;
+                            break;
+                        }
+                        if (dizi[j].Contains(denklem2))
+                        {
+                            sayi++;
+                            break;
+                        }
+                        j++;
+                    }
+                }
+                if (kontrol == false)
+                {
+                    break;
+                }
+            }
+
+            if (kontrol == false)
+            {
+                richTextBox1.Text += "\n\nDenklem numarası ve bölüm başlığı arasında uyuşmazlık vardır.";
+            }
+        }
+
+        public void tabloKontrol(string metin)
+        {
+
+        }
+    }    
+}
